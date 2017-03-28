ffeddc9b3087c6ebcc84706aced4f3e8860a3158
Yuri Pereira <yuri.souza@al.infnet.edu.br>
2017-03-27 21:45:49 -0300

    Adiciona repositorios

---
+++ C#/MustDo.Solution/MustDo.Infra.Data/Repositories/ComentarioRepository.cs
@@ -0,0 +1,14 @@
+﻿using MustDo.Domain.Entities;
+using MustDo.Domain.Interfaces.Repositories;
+using MustDo.Infra.Data.Contexts;
+
+namespace MustDo.Infra.Data.Repositories
+{
+	public class ComentarioRepository : RepositoryBase<Comentario>, IComentarioRepository
+	{
+		public ComentarioRepository(MustDo4EntitiesDb db) : base(db)
+		{
+
+		}
+	}
+}
---
+++ C#/MustDo.Solution/MustDo.Infra.Data/Repositories/TarefaRepository.cs
@@ -0,0 +1,14 @@
+﻿using MustDo.Domain.Entities;
+using MustDo.Domain.Interfaces.Repositories;
+using MustDo.Infra.Data.Contexts;
+
+namespace MustDo.Infra.Data.Repositories
+{
+	public class TarefaRepository : RepositoryBase<Tarefa>, ITarefaRepository
+	{
+		public TarefaRepository(MustDo4EntitiesDb db) : base(db)
+		{
+
+		}
+	}
+}
