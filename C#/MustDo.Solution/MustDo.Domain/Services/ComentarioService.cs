435b9c1ec1a81dcda6bed1f180be797d47e78783
Yuri Pereira <yuri.souza@al.infnet.edu.br>
2017-03-27 21:40:27 -0300

    Adiciona interfaces de serviço e repositório. Adiciona classes de serviços.

---
+++ C#/MustDo.Solution/MustDo.Domain/Services/ComentarioService.cs
@@ -0,0 +1,14 @@
+﻿using MustDo.Domain.Entities;
+using MustDo.Domain.Interfaces.Repositories;
+using MustDo.Domain.Interfaces.Services;
+
+namespace MustDo.Domain.Services
+{
+	public class ComentarioService : ServiceBase<Comentario>, IComentarioService
+	{
+		public ComentarioService(IComentarioRepository comentarioRepository) : base(comentarioRepository)
+		{
+
+		}
+	}
+}
---
+++ C#/MustDo.Solution/MustDo.Domain/Services/TarefaService.cs
@@ -0,0 +1,14 @@
+﻿using MustDo.Domain.Entities;
+using MustDo.Domain.Interfaces.Repositories;
+using MustDo.Domain.Interfaces.Services;
+
+namespace MustDo.Domain.Services
+{
+	public class TarefaService : ServiceBase<Tarefa>, ITarefaService
+	{
+		public TarefaService(ITarefaRepository tarefaRepository) : base(tarefaRepository)
+		{
+
+		}
+	}
+}
