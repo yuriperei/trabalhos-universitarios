951846dce7303f58c7ef31e52ef770ec84187e1e
yuriperei <yuri.souza@al.infnet.edu,br>
2018-03-25 20:51:46 -0300

    :art: Insere nova entidade

---
+++ Javascript/Angular/src/domain/entities/competencia.ts
@@ -0,0 +1,10 @@
+export class Competencia{
+    public id: string;
+    public description: string;
+    public nota: string;
+
+    constructor(id, description){
+        this.id = id;
+        this.description = description;
+    }
+}
