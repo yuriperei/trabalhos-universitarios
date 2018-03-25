c15a7af351337ff22a4d9b73ef06abee0d5a62b2
yuriperei <yuri.souza@al.infnet.edu,br>
2018-03-25 20:52:09 -0300

    :art: Insere imports necessarios

--- Javascript/Angular/src/app/app.module.ts
+++ Javascript/Angular/src/app/app.module.ts
@@ -1,26 +1,40 @@
+import { ApiInfnetService } from './../domain/services/apiinfnet.service';
+import { RouterModule } from '@angular/router';
+import { HttpModule } from '@angular/http';
+import { CommonModule } from '@angular/common';
+import { UsuariologinComponent } from './../pages/usuario/login/usuariologin.component';
+import { UsuarioperfilComponent } from './../pages/usuario/perfil/usuarioperfil.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { UsuarioModule } from './../pages/usuario/usuario.module';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app.routes';
 
 import 'rxjs/add/operator/map';
+import { HeaderComponent } from '../pages/header/header.component';
+import { MateriaComponent } from '../pages/usuario/materia/materia.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    UsuariologinComponent,
+    UsuarioperfilComponent,
+    MateriaComponent
   ],
   imports: [
     NgbModule.forRoot(),
-    FormsModule,
+    FormsModule, ReactiveFormsModule,
     AppRoutingModule,
     BrowserModule,
-	  UsuarioModule
+    CommonModule,
+    FormsModule,
+    HttpModule,
+    RouterModule
   ],
-  providers: [],
+  providers: [ApiInfnetService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
